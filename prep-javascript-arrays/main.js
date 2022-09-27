c25a84f7f9e83a66194b9f4a03ecf2c46c2e7389
Alexandra Christensen <alexmariechristensen@gmail.com>
2022-09-27 23:26:14 +0000

    prep-javascript-arrays.

---
+++ prep-javascript-arrays/main.js
@@ -0,0 +1,15 @@
+const languages = ['html', 'CSS', 'javascript', 'react', 'node', 'postgres'
+];
+console.log(languages);
+
+var firstElement = languages[0];
+console.log('the firstElement:', firstElement);
+
+var thirdElement = languages[2];
+console.log('the third element is:', thirdElement);
+
+var length = languages.length;
+console.log('the length of the array is:', length);
+
+var lastelement = languages[languages.length - 1];
+console.log('the last element of the array is:', lastelement);
